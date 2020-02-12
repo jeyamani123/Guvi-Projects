79391101ed628fcb8639903f2aa0e707b2118aee
Bala Iyyappan V <43007978+BalaIyyappan@users.noreply.github.com>
2020-02-13 00:14:58 +0530

    Update Flag.js

--- Country Flag and Details/Flag.js
+++ Country Flag and Details/Flag.js
@@ -3,9 +3,10 @@ fetch("https://restcountries.eu/rest/v2/all")
 .then((out)=>{
     //console.log(out); //To display all arrays
     
-    for(var i=0;i<21;i++){
+    for(var i=1;i<21;i++){
         var image=out[i]["flag"];
-        document.getElementById("img${i}").setAttribute("src",image);
+        var id="img"+i;
+        document.getElementById(id).setAttribute("src",image);
 
         console.log(out[i]);
     }
