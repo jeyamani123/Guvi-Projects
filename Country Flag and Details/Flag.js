9073010ee89c9cd0c1afecf9036841c68cc44df2
Bala Iyyappan V <43007978+BalaIyyappan@users.noreply.github.com>
2020-02-14 22:15:16 +0530

    Update Flag.js

--- Country Flag and Details/Flag.js
+++ Country Flag and Details/Flag.js
@@ -3,11 +3,22 @@ fetch("https://restcountries.eu/rest/v2/all")
 .then((out)=>{
     //console.log(out); //To display all arrays
     
+    let divid=document.getElementById("card"); //Getting id of parent div
     for(var i=1;i<21;i++){
-        var image=out[i]["flag"];
-        var id="img"+i;
+        var div=document.createElement('div'); //Creating div for every iteration
+    	divid.appendChild(div);                //Appending div to hml
+    	//console.log(div);
+    	var img=document.createElement("img"); //Creating img for every iteration
+    	div.appendChild(img);                  //Append after creation
+    	img.setAttribute("alt","Image");       //Set attribute for the appended img tag 
+    	var id="img"+i;
+    	img.setAttribute("id",id)
+    	var image=out[i]["flag"];
         document.getElementById(id).setAttribute("src",image);
-
+        //creating button 
+        let btn="button"+"("+i+")";
+        img.setAttribute("onclick",btn);        //To set onclick attribute for image tag
+        
         console.log(out[i]);
     }
 
